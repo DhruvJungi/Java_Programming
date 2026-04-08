5cac10fa777aa5036ad82e2e48a894109a5e37c0
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-04-08 19:40:34 +0530

    java basic 4 completed

--- loops/JavaBasic4.java
+++ loops/JavaBasic4.java
@@ -130,7 +130,7 @@ public class JavaBasic4 {
             System.out.println("n is prime");
         } else {
             boolean isPrime = true;
-            for (int i = 10; i <= Math.sqrt(n); i++) {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
                 if (n % i == 0) { // n is a multiple of i
                     isPrime = false;
                     break; // optimization: stop once a divisor is found
