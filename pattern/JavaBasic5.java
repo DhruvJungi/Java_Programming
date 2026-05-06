0ede7f7256873c576499037b32d5b2c8d068bfbe
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-05-06 18:59:15 +0530

    started advance pattern

--- pattern/JavaBasic5.java
+++ pattern/JavaBasic5.java
@@ -29,14 +29,43 @@ public class JavaBasic5 {
         // }
 
         // pattern-4
-        int n =4;
-        char ch ='a';
-        for(int line = 1; line <= n; line++){
-            for(int charac = 1; charac <= line; charac++){
-                System.out.print(ch);
-                ch++;
+        // int n =4;
+        // char ch ='a';
+        // for(int line = 1; line <= n; line++){
+        //     for(int charac = 1; charac <= line; charac++){
+        //         System.out.print(ch);
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // Practice ques 
+        // pattern-1
+        // int row=4;
+        // int cols=5;
+        // for(int line = 1; line <= row; line++){
+        //     for(int star = 1; star <= cols; star++){
+        //         if( line == 1 || line == row || star == 1 || star == cols){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // pattern-2
+        int n = 4;
+         for (int i = 1; i <= n; i++) {
+            // print spaces first
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
-            System.out.println();
+            // then print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
         }
     }
 }
